87d3ec77979d2e9b97860f27a0722a076f0052c4
kahyun0255 <kkh9946@naver.com>
2026-06-08 21:16:22 +0900

    git init

---
+++ src/main/java/com/meditrip/MeditripApplication.java
@@ -0,0 +1,13 @@
+package com.meditrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeditripApplication.class, args);
+    }
+
+}
